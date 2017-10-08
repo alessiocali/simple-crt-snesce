e9141c6b7f83d147fee1109325d575b7a4e9ff5b
Alessio Giuseppe Calì <alessiog.cali@hotmail.com>
2017-10-08 21:35:02 +0200

    Add files via upload

---
+++ simple_scanline.glsl
@@ -0,0 +1,55 @@
+#pragma parameter scanLineScale "Scan Line Scale" 1.0 0.5 3.0 0.5
+#pragma parameter blendExponent "Blend Exponent" 0.3 0.1 1.0 0.1
+
+#ifndef PARAMETER_UNIFORM
+#define scanLineScale 1.0
+#define blendExponent 0.3
+#endif
+
+precision mediump float;
+
+#if defined(FRAGMENT)
+
+varying vec2 vTexCoord;
+varying vec2 vScanline;
+
+uniform sampler2D Texture;
+#ifdef PARAMETER_UNIFORM
+uniform float scanLineScale;
+uniform float blendExponent;
+#endif
+
+#define hScale 224.0
+
+vec4 powv(vec4 base, float ex) 
+{
+	return vec4(pow(base.x, ex), pow(base.y, ex), pow(base.z, ex), pow(base.w, ex));
+}
+
+void main()
+{
+    vec4 col = texture2D(Texture, vTexCoord);
+	vec2 tScanline = vScanline;
+	tScanline.y = mod(floor(tScanline.y * hScale * scanLineScale), 2.0);
+	tScanline.x = 1.0 - tScanline.y;
+	
+    gl_FragColor = (tScanline.x + tScanline.y * powv(col, blendExponent)) * col;
+}
+
+#elif defined(VERTEX)
+
+varying vec2 vTexCoord;
+varying vec2 vScanline;
+
+attribute vec4 VertexCoord;
+attribute vec4 TexCoord;
+
+uniform mat4 MVPMatrix;
+
+void main()
+{
+	gl_Position = MVPMatrix * VertexCoord;
+	vScanline = VertexCoord.xy;
+	vTexCoord = TexCoord.xy;
+}
+#endif
